cdb52f96b023694e14f851e855c85543816ac37d
chdavidson <65898287+chdavidson@users.noreply.github.com>
2021-08-04 10:12:44 +0100

    again.. removing submodule, directory name change client > front end

---
+++ Implementation/Frontend/Screens/AccountSettingsScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, View, Text} from 'react-native'
+
+const AccountSettingsScreen = () => {
+    return(
+        <Text>Account Settings Screen</Text>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default AccountSettingsScreen;
