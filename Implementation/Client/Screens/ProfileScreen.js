2094bce5675db362fa0183864cdcf107d156b4b2
chdavidson <65898287+chdavidson@users.noreply.github.com>
2021-08-07 10:26:12 +0100

    personalised transaction list rendering on account page - absolute basic

--- Implementation/Client/Screens/ProfileScreen.js
+++ Implementation/Client/Screens/ProfileScreen.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect, useContext} from 'react'
-import { View, Text, Button, StyleSheet } from 'react-native'
+import { View, Text, Button, StyleSheet, FlatList } from 'react-native'
 import ColourPalette from '../Constants/ColourPalette'
 import ProfilePicture from '../components/ProfilePicture'
 import SettingsClickable from '../components/SettingsClickable'
@@ -8,29 +8,58 @@ import { DbContext } from "../provider/DbProvider";
 
 
 const ProfileScreen = ({route, navigation}) => {
-    // const {users} = route.params;
-    // const userID = "saadtarik"
 
-    // const auth = useContext(AuthContext);
-    // const userData = auth.userData;
+
+    const [transactions, setTransactions] = useState(null);
 
     const db = useContext(DbContext);
-    // console.log("db: " + db)
+   
     const currentUser = db.currentUser
 
     useEffect(() => {
         if(currentUser){        
             console.log(currentUser.firstName)
+            displayTransactions();
         }
         else(
             console.log("NAEBODY HERE")
         )
     },[currentUser])
 
+    const displayTransactions = () => {
+        console.log(currentUser.expenses);
+        let payList = (currentUser.payslips.map((p,i) => {
+                            return(
+                                    <View key={i}>
+                                        <Text>£{p.amount}</Text>
+                                        <Text>{p.companyName}</Text>
+                                        <Text>{p.invoiceNumber}</Text>
+                                        <Text>{p.date}</Text>
+                                    </View>
+                            )
+        }))
+        let expenseList = (currentUser.expenses.map((e, i) => {
+                                return(
+                                    <View key={i+currentUser.payslips.length}>
+                                        <Text>£{e.amount}</Text>
+                                        <Text>{e.category}</Text>
+                                        <Text>{e.date}</Text>
+                                    </View>
+
+                                )
+        }))
+
+
+        // Maybe sort by date first?
+
+        return[...payList,...expenseList];
+    }
+
     return(
         <View style={styles.screen}>
             <View style={styles.userSummaryContainer}>
                 {currentUser ? <Text>{currentUser.firstName}</Text> : null}
+                    {currentUser ? <View>{displayTransactions()}</View> : null}
             </View>
         </View>
     )
@@ -42,6 +71,10 @@ const styles = StyleSheet.create({
         padding: 10,
         width: '100%',
         backgroundColor: ColourPalette.SECONDARY,
+    },
+    transactionList:{
+        width: '100%',
+        height: 40
     },
     profilePictureContainer: {
         alignItems: 'center',
