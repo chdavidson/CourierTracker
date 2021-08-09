ce8c1adc545f86b621c35e047ce817a8072218e3
michael403053 <michael.christie5@hotmail.co.uk>
2021-08-09 11:27:23 +0100

    merge mostly sucessful

--- Implementation/Client/navigation/AppNavigator.js
+++ Implementation/Client/navigation/AppNavigator.js
@@ -3,12 +3,25 @@ import firebase from "firebase/app";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  View,
+  Image,
+  TouchableOpacity,
+  Text,
+  StyleSheet
+} from "react-native";
+import { COLORS, FONTS, icons } from "../Constants"
 
 import { useTheme, themeColor } from "react-native-rapi-ui";
 import TabBarIcon from "../components/utils/TabBarIcon";
 import TabBarText from "../components/utils/TabBarText";
 //Screens
 import Home from "../Screens/Home";
+import Settings from "../Screens/Settings";
+import ReportDetailScreen from "../Screens/ReportDetailScreen";
+import Report from "../Screens/Report";
+import RecordData from "../Screens/RecordData";
+import HomeScreen from "../Screens/HomeScreen";
 import About from "../Screens/About";
 import ProfileScreen from "../Screens/ProfileScreen";
 import Loading from "../Screens/utils/Loading";
@@ -17,6 +30,7 @@ import AccountSettingsScreen from "../Screens/AccountSettingsScreen"
 import ReportScreen from "../Screens/ReportScreen"
 import RecordItemScreen from "../Screens/RecordItemScreen"
 
+import { LinearGradient } from 'expo-linear-gradient';
 
 // Auth Screens
 import Login from "../Screens/auth/Login";
@@ -63,14 +77,17 @@ const Main = () => {
       }}
     >
       <MainStack.Screen name="MainTabs" component={MainTabs} />
-      <MainStack.Screen name="Landing Page" component={LandingScreen}/>
-        <MainStack.Screen name="Profile" component={ProfileScreen} />
-        <MainStack.Screen name="Reports" component={ReportScreen} />
-        <MainStack.Screen name="Record" component={RecordItemScreen} />
+      <MainStack.Screen name="Landing Page" component={HomeScreen}/>
+      <MainStack.Screen name="Profile" component={ProfileScreen} />
+      <MainStack.Screen name="Reports" component={ReportDetailScreen} />
+      <MainStack.Screen name="Record" component={RecordItemScreen} />
+      <MainStack.Screen name="RecordData" component={RecordData} />
               
     </MainStack.Navigator>
   );
 };
+
+
 
 const Tabs = createBottomTabNavigator();
 const MainTabs = () => {
@@ -85,10 +102,11 @@ const MainTabs = () => {
         },
       }}
     >
-      {/* these icons using Ionicons */}
+
+
       <Tabs.Screen
         name="Home"
-        component={LandingScreen}
+        component={HomeScreen}
         options={{
           tabBarLabel: ({ focused }) => (
             <TabBarText focused={focused} title="Home" />
@@ -112,7 +130,7 @@ const MainTabs = () => {
       />
       <Tabs.Screen
         name="Reports"
-        component={ReportScreen}
+        component={ReportDetailScreen}
         options={{
           tabBarLabel: ({ focused }) => (
             <TabBarText focused={focused} title="Reports" />
@@ -134,11 +152,206 @@ const MainTabs = () => {
           ),
         }}
       />
+      <Tabs.Screen
+        name="RecordData"
+        component={RecordData}
+        options={{
+          tabBarLabel: ({ focused }) => (
+            <TabBarText focused={focused} title="RecordData" />
+          ),
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} icon={"ios-barcode-outline"} />
+          ),
+        }}
+      />
               
        
     </Tabs.Navigator>
   );
 };
+
+
+// const Tab = createBottomTabNavigator()
+// const TabBarCustomButton = ({children, onPress}) => {
+//     return(
+//         <TouchableOpacity
+//             style={{
+//                 top: -30,
+//                 justifyContent: 'center',
+//                 alignItems: 'center',
+//                 paddingTop: 30,
+//                 ...styles.shadow
+//             }}
+//             onPress={onPress}
+//          >
+//              <LinearGradient
+//                 colors={[COLORS.primary, COLORS.secondary]}
+//                 style={{
+//                     width: 70,
+//                     height: 70,
+//                     borderRadius: 35
+//                 }}
+//              >
+//                  {children}
+//              </LinearGradient>
+//          </TouchableOpacity>
+//     )
+// }
+
+// const Tabs = () => {
+//     return (
+//         <Tab.Navigator 
+//             tabBarOptions={{
+//                 showLabel: false,
+//                 style: {
+//                     position: 'absolute',
+//                     bottom: 0,
+//                     left: 0,
+//                     right: 0,
+//                     elevation: 0,
+//                     backgroundColor: COLORS.white,
+//                     borderTopColor: "transparent"
+//                 }
+//             }}
+//             screenOptions={{
+//                 headerShown: null
+//             }}
+//         >
+//             <Tab.Screen
+//                 name="Home"
+//                 component={Home}
+//                 options={{
+//                     tabBarIcon: ({focused}) => (
+//                         <View style={{alignItems: 'center',
+//                             justifyContent: 'center',
+//                             paddingTop: 20
+//                         }}>
+//                         <Image 
+//                             source={icons.home}
+//                             resizeMode="contain"
+//                             style={{
+//                                 width: 30,
+//                                 height: 30,
+//                                 tintColor: focused ? COLORS.primary
+//                                 : COLORS.black
+//                             }}
+                    
+//                          />
+//                          <Text style={{color : focused ? COLORS.primary : COLORS.black,
+//                             ...FONTS.body5
+//                          }}>Home</Text>
+
+//                         </View>
+//                     )
+//                 }}
+//             />
+//             <Tab.Screen
+//                 name="Portfolio"
+//                 component={Report}
+//                 options={{
+//                     tabBarIcon: ({focused}) => (
+//                         <View style={{alignItems: 'center',
+//                             justifyContent: 'center',
+//                             paddingTop: 20
+//                         }}>
+//                         <Image 
+//                             source={icons.pie_chart}
+//                             resizeMode="contain"
+//                             style={{
+//                                 width: 30,
+//                                 height: 30,
+//                                 tintColor: focused ? COLORS.primary
+//                                 : COLORS.black
+//                             }}
+                    
+//                          />
+//                          <Text style={{color : focused ? COLORS.primary : COLORS.black,
+//                             ...FONTS.body5
+//                          }}>Reports</Text>
+
+//                         </View>
+//                     )
+//                 }}
+//             />
+//             <Tab.Screen
+//                 name="Transaction"
+//                 component={RecordData}
+//                 options={{
+//                     tabBarIcon: ({focused}) => (
+//                         <Image
+//                             source={icons.transaction}
+//                             resizeMode= 'contain'
+//                             style={{
+//                                 width: 30,
+//                                 height: 30,
+//                                 tintColor: COLORS.white,
+//                             }} />
+//                     ),
+//                     tabBarButton: (props) => (
+//                         <TabBarCustomButton {...props}/>
+//                     )
+//                 }}
+                
+//             />
+//             <Tab.Screen
+//                 name="Details"
+//                 component={Detail}
+//                 options={{
+//                     tabBarIcon: ({focused}) => (
+//                         <View style={{alignItems: 'center',
+//                             justifyContent: 'center',
+//                             paddingTop: 20
+//                         }}>
+//                         <Image 
+//                             source={icons.line_graph}
+//                             resizeMode="contain"
+//                             style={{
+//                                 width: 30,
+//                                 height: 30,
+//                                 tintColor: focused ? COLORS.primary
+//                                 : COLORS.black
+//                             }}
+                    
+//                          />
+//                          <Text style={{color : focused ? COLORS.primary : COLORS.black,
+//                             ...FONTS.body5
+//                          }}>Stats</Text>
+
+//                         </View>
+//                     )
+//                 }}
+//             />
+//             <Tab.Screen
+//                 name="Settings"
+//                 component={Settings}
+//                 options={{
+//                     tabBarIcon: ({focused}) => (
+//                         <View style={{alignItems: 'center',
+//                             justifyContent: 'center',
+//                             paddingTop: 20
+//                         }}>
+//                         <Image 
+//                             source={icons.settings}
+//                             resizeMode="contain"
+//                             style={{
+//                                 width: 30,
+//                                 height: 30,
+//                                 tintColor: focused ? COLORS.primary
+//                                 : COLORS.black
+//                             }}
+                    
+//                          />
+//                          <Text style={{color : focused ? COLORS.primary : COLORS.black,
+//                             ...FONTS.body5
+//                          }}>Settings</Text>
+
+//                         </View>
+//                     )
+//                 }}
+//             />
+//         </Tab.Navigator>
+//     )
+// }
 
 export default () => {
   const auth = useContext(AuthContext);
